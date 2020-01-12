6a556b4b7eb893ac246119e1ed9fae2ea2c80cbe
Caleb <48451841+CalebThePerson@users.noreply.github.com>
2020-01-11 18:24:10 -0800

    Waifus
    Just making fixing some bugs and adding color when you get it wrong

--- GuessThatWaifu/GuessThatWaifu/ContentView.swift
+++ GuessThatWaifu/GuessThatWaifu/ContentView.swift
@@ -9,15 +9,24 @@
 import SwiftUI
 
 
+
 struct ContentView: View {
     @State private var Waifus = ["Rem","Chika","Zero Two","SpeedWagon","Ochaco","Momo","Nezuko","Nami","YunYun","Megumin","Darkness","Reigen","Diane","Froppy"].shuffled()
+    
     @State private var CorrectGirl = Int.random(in: 0...2)
     
     @State private var ShowingScore = false
     @State private var ScoreTitle = ""
     @State private var Points = 0
     
+    
+    
+    @State private var useRed:Bool = false
+    @State private var useGreen:Bool = false
+    
     var body: some View {
+
+                
         ZStack {
             Image("Testing")
                 .resizable()
@@ -25,34 +34,37 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
 
             VStack(spacing: 30) {
-                        
+                            
                 VStack {
-                    Spacer()
                     Text("Tap The Waifu")
-                        .foregroundColor(.white)
+                        .foregroundColor(useRed ? .red:.white)
                         .font(.largeTitle)
+                        
+                        
                     Text(Waifus[CorrectGirl])
-                        .foregroundColor(.white)
+                        .foregroundColor(useRed ? .red: .white)
                         .font(.largeTitle)
                         .fontWeight(.black)
-                            
+                                
                 }
-    
-                
+        
+                    
                 ForEach(0 ..< 4 ) { number in
                     Button(action: {
                         self.WaifuTapped(number)
                     }) {
+
+                            
                         Image(self.Waifus[number])
                             .renderingMode(.original)
-                            .frame(width: 90, height: 90)
+                            .frame(width: 91, height: 91)
                         .clipShape(Circle())
-                            .overlay(Circle().stroke(Color.white, lineWidth : 4))
-                       
-                        
+                            .overlay(Circle().stroke(Color.white , lineWidth : 4))
+                           
+                            
 
                     }
-                    
+    
                 }
                 Text("You have")
                     .font(.largeTitle)
@@ -61,26 +73,30 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .font(.largeTitle)
                     .fontWeight(.black)
-                        
+                            
                 Spacer()
             }
         }
         .alert(isPresented: $ShowingScore) {
             Alert(title: Text(ScoreTitle),message: Text("Your Score is \(Points)"),dismissButton:
                 .default(Text("Countinue Weeb")) {
+                    self.useRed = false
                     self.askQuestion()
-                })
+                    })
         }
         
     }
-    //.background(Color.black)
+
     
     func WaifuTapped(_ number : Int) {
         if number == CorrectGirl {
+            useRed = false
             ScoreTitle = "Correct"
             Points += 1
         } else {
             ScoreTitle = "Wrong, The Waifu was \(self.Waifus[number])"
+            Points = 0
+            useRed = true
         }
                 
         ShowingScore = true
